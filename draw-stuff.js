f44e76b1db0141e0242b1573927bbe43aad0b177
jcastillo2583 <bluejccastle@gmail.com>
2019-04-15 16:59:06 -0700

    Add files via upload

---
+++ draw-stuff.js
@@ -0,0 +1,45 @@
+// Draw stuff
+// Time-stamp: <2019-04-24 8:00AM JVJ>
+// ------------------------------------------------------------
+
+// FUN. Draw filled rect.
+function draw_rect( ctx, x, y, fill) 
+{
+	x = x || 0;
+	y = y || 0;
+    fill = fill || "white";
+    ctx.save( );
+    ctx.fillStyle = fill;
+    ctx.rect(x * 10, y * 10, 10, 10);
+    ctx.fill();
+    ctx.restore( );
+}
+
+// =====================================================  draw_grid ====
+function draw_grid( rctx, rminor, rmajor, rstroke, rfill  ) 
+{
+    rctx.save( );
+    rctx.strokeStyle = rstroke;
+    rctx.fillStyle = rfill;
+    let width = rctx.canvas.width;
+    let height = rctx.canvas.height;
+    for ( var ix = 0; ix < width; ix += rminor )
+    {
+        rctx.beginPath( );
+        rctx.moveTo( ix, 0 );
+        rctx.lineTo( ix, height );
+        rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.5;
+        rctx.stroke( );
+        if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
+    }
+    for ( var iy = 0; iy < height; iy += rminor )
+    {
+        rctx.beginPath( );
+        rctx.moveTo( 0, iy );
+        rctx.lineTo( width, iy );
+        rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.5;
+        rctx.stroke( );
+        if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
+    }
+    rctx.restore( );
+}
