f55580b5ee085ced183b0d06ccbddbf1b7bebdc9
jcastillo2583 <bluejccastle@gmail.com>
2019-05-04 11:13:21 -0700

    Update Edge.js

--- Edge.js
+++ Edge.js
@@ -1,11 +1,11 @@
 class Edge
 {
-	constructor(firstCell, secondCell, flow)
+	constructor(firstCell, secondCell)
 	{
 		this.firstCell = firstCell;
 		this.secondCell = secondCell;
         
-        this.flow = flow;
+        this.flow;
         this.capacity = (firstCell.capacity + secondCell.capacity ) / 2;
 	}
 	
@@ -16,18 +16,16 @@ class Edge
 	 
 	placeEdge()
 	{
-		console.log("Placing edge from (" + this.firstCell.i + ", " + this.firstCell.j + " to " + this.secondCell.i + ", " + this.secondCell.j + ")");
+		console.log("Placing edge from (" + this.firstCell.i + ", " + this.firstCell.j + ") to (" + this.secondCell.i + ", " + this.secondCell.j + ")");
 		draw_line(context, this.firstCell, this.secondCell);
 		if (!(this.firstCell === source || this.firstCell === sink))
 		{
 			draw_dot(context, this.firstCell.x, this.firstCell.y);
-			this.firstCell.marked = true;
 		}
     
 		if (!(this.secondCell === source || this.secondCell === sink))
 		{
 			draw_dot(context, this.secondCell.x, this.secondCell.y);
-			this.secondCell.marked = true;
 		}
 	}
 }
